3a0e5a4815b4aceaf3c0005ef643f1c7e859b051
Parajanya Shrestha <parajanya.shrestha@bajratechnologies.com>
2024-02-20 14:32:47 +0545

    feat: Display the menu options for CRUD and Exit

--- expense-tracker/expense_tracker.rb
+++ expense-tracker/expense_tracker.rb
@@ -10,9 +10,43 @@ def establish_database_connection
   end
 end
 
-conn = establish_database_connection
-results = conn.exec('SELECT * FROM expenses')
-results.each do |row|
-  p row['item_name']
+# conn = establish_database_connection
+# results = conn.exec('SELECT * FROM expenses')
+# results.each do |row|
+#   p row['item_name']
+# end
+# conn.close
+
+
+def display_menu 
+  puts "Make your choice for expense tracker"
+  puts "1. Add new expense"
+  puts "2. Read existing expenses"
+  puts "3. Update Expense"
+  puts "4. Delete Expense"
+  puts "5. Exit"
+end 
+
+def valid_input?(input)
+  input.match?(/^[1-5]$/)
 end
-conn.close
+
+def get_valid_choice
+  input = nil
+  loop do
+    print "Enter a valid choice in range (1-5)"
+    input = gets.chomp
+    if valid_input?(input)
+      input = input.to_i
+      if input >= 1 && input <=5
+        break
+      end
+    end
+    puts "Invalid input. Please enter a number between 1 and 5."
+  end
+  input = input.to_i
+  return input
+end
+
+display_menu
+get_valid_choice
